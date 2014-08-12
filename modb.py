709196b531d4dfb30b9c3800ce607773ca483093
Anthony Corriero <anthony.corriero@gmail.com>
2014-08-12 11:58:03 -0400

    just more stuff

---
+++ modb.py
@@ -0,0 +1,6 @@
+X = 11
+
+print X
+from moda import X, f
+f()
+print X
---
+++ module2.py
@@ -0,0 +1,8 @@
+print 'starting to load'
+import sys
+name = 42
+
+def fun(): pass
+class klass: pass
+
+print 'done loading.'
